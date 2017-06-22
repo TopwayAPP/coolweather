6e2df09e2fd6978059d8ec225990e539e295b885
wangtl <wangtl@gdtopway.com>
2017-06-22 21:43:28 +0800

    修改显示不正确代码

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by wangt on 2017-06-22.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
